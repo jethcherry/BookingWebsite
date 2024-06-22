fc89747da222d3dc01a5e04c60434d36f367c9e2
Jeth_Dev <jethrosumbeiywet@gmail.com>
2024-06-22 22:59:58 +0300

    yes

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,14 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
   slideIndex = 0;
+
+  slides = [
+    { src: '../../assets/tours/parisFrance.jpeg', alt: 'Slide 1' },
+    { src: '../../assets/tours/portugal.jpeg', alt: 'Slide 2' },
+    { src: '../../assets/tours/turkey.jpeg', alt: 'Slide 3' },
+    { src: '../../assets/tours/greece.jpeg', alt: 'Slide 4' }
+  ];
 
   constructor() { }
 
@@ -16,6 +24,8 @@ export class HomeComponent implements OnInit{
     this.showSlides();
   }
   
+ 
+ 
   showSlides(): void {
     const slides = document.getElementsByClassName("slide") as HTMLCollectionOf<HTMLElement>;
     for (let i = 0; i < slides.length; i++) {
